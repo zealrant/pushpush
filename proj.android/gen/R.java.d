819d4d2080144c05d849ae0d8449392944917fe1
beonit <beonit@gmail.com>
2014-04-16 22:43:27 +0900

    remove project file. tracking only class and resource file

--- proj.android/gen/R.java.d
+++
@@ -1,9 +0,0 @@
-/home/beonit/work/pushpush/pushpush/proj.android/gen/com/yasik/pushpush/R.java \
- : /home/beonit/work/pushpush/pushpush/proj.android/res/drawable-ldpi/icon.png \
-/home/beonit/work/pushpush/pushpush/proj.android/res/drawable-hdpi/icon.png \
-/home/beonit/work/pushpush/pushpush/proj.android/res/values/strings.xml \
-/home/beonit/work/pushpush/pushpush/proj.android/res/drawable-mdpi/icon.png \
-/home/beonit/work/pushpush/pushpush/proj.android/bin/res/drawable-ldpi/icon.png \
-/home/beonit/work/pushpush/pushpush/proj.android/bin/res/drawable-hdpi/icon.png \
-/home/beonit/work/pushpush/pushpush/proj.android/bin/res/drawable-mdpi/icon.png \
-/home/beonit/work/pushpush/pushpush/proj.android/bin/AndroidManifest.xml \
